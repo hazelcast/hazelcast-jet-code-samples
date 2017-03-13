7f4e7bdf0e4c86352015fc834a239692586c99f1
Marko Topolnik <marko@hazelcast.com>
2017-03-13 17:58:07 +0300

    Clean up ConsumeKafka

--- kafka/consume-kafka/src/main/java/ConsumeKafka.java
+++ kafka/consume-kafka/src/main/java/ConsumeKafka.java
@@ -1,13 +1,28 @@
-import com.hazelcast.jet.AbstractProcessor;
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.hazelcast.jet.DAG;
 import com.hazelcast.jet.Jet;
 import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.Job;
 import com.hazelcast.jet.Vertex;
 import com.hazelcast.jet.config.InstanceConfig;
 import com.hazelcast.jet.config.JetConfig;
 import com.hazelcast.jet.connector.kafka.ReadKafkaP;
 import com.hazelcast.jet.stream.IStreamMap;
-import com.hazelcast.map.listener.EntryAddedListener;
 import kafka.admin.RackAwareMode;
 import kafka.server.KafkaConfig;
 import kafka.server.KafkaServer;
@@ -25,24 +40,23 @@ import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Properties;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.hazelcast.jet.Edge.between;
 import static com.hazelcast.jet.Processors.writeMap;
 import static com.hazelcast.jet.connector.kafka.ReadKafkaP.readKafka;
 import static java.lang.Runtime.getRuntime;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static kafka.admin.AdminUtils.createTopic;
 
 /**
- * A sample which does a distributed read from 2 Kafka topics and writes to an IMap
+ * A sample which does a distributed read from 2 Kafka topics and writes to an
+ * IMap.
  * <p>
  * {@link ReadKafkaP} is a processor that can be used for reading from Kafka.
- * High level consumer API is used to subscribe the topics which will
+ * High-level consumer API is used to subscribe to the topics which will
  * do the assignments of partitions to consumers (processors)
  */
 public class ConsumeKafka {
@@ -57,7 +71,9 @@ public class ConsumeKafka {
         new ConsumeKafka().run();
     }
 
-    private void run() throws IOException, InterruptedException {
+    private void run() throws Exception {
+        System.setProperty("hazelcast.logging.type", "log4j");
+
         createKafkaCluster();
         fillTopics();
 
@@ -67,66 +83,65 @@ public class ConsumeKafka {
 
         JetInstance instance = Jet.newJetInstance(cfg);
         Jet.newJetInstance(cfg);
-
-        DAG dag = new DAG();
-        Vertex source = dag.newVertex("source", readKafka(getProperties(), "t1", "t2"));
-        Vertex sink = dag.newVertex("sink", writeMap("sink"));
-
-        dag.edge(between(source, sink));
-
         IStreamMap<String, Integer> sinkMap = instance.getMap("sink");
 
-        long start = System.currentTimeMillis();
-        instance.newJob(dag).execute();
+        Job job = createJetJob(instance);
+        long start = System.nanoTime();
+        job.execute();
         while (true) {
             int mapSize = sinkMap.size();
-            System.out.println("Received " + mapSize + " entries in " + (System.currentTimeMillis() - start) + "ms.");
+            System.out.format("Received %d entries in %d milliseconds.%n",
+                    mapSize, NANOSECONDS.toMillis(System.nanoTime() - start));
             if (mapSize == MESSAGE_COUNT_PER_TOPIC * 2) {
                 break;
             }
             Thread.sleep(100);
         }
-
         shutdownKafkaCluster();
         System.exit(0);
-}
+    }
 
-    /**
-     * Creates an embedded zookeeper server and a kafka broker
-     *
-     * @throws IOException
-     */
+    private static Job createJetJob(JetInstance instance) {
+        DAG dag = new DAG();
+        Properties props = props(
+                "group.id", "group-" + Math.random(),
+                "bootstrap.servers", "localhost:9092",
+                "key.deserializer", StringDeserializer.class.getCanonicalName(),
+                "value.deserializer", IntegerDeserializer.class.getCanonicalName(),
+                "auto.offset.reset", "earliest");
+        Vertex source = dag.newVertex("source", readKafka(props, "t1", "t2"));
+        Vertex sink = dag.newVertex("sink", writeMap("sink"));
+        dag.edge(between(source, sink));
+        return instance.newJob(dag);
+    }
+
+    // Creates an embedded zookeeper server and a kafka broker
     private void createKafkaCluster() throws IOException {
         zkServer = new EmbeddedZookeeper();
         String zkConnect = "localhost:" + zkServer.port();
         ZkClient zkClient = new ZkClient(zkConnect, 30000, 30000, ZKStringSerializer$.MODULE$);
         zkUtils = ZkUtils.apply(zkClient, false);
 
-        Properties brokerProps = new Properties();
-        brokerProps.setProperty("zookeeper.connect", zkConnect);
-        brokerProps.setProperty("broker.id", "0");
-        brokerProps.setProperty("log.dirs", Files.createTempDirectory("kafka-").toAbsolutePath().toString());
-        brokerProps.setProperty("listeners", "PLAINTEXT://localhost:9092");
-        KafkaConfig config = new KafkaConfig(brokerProps);
+        KafkaConfig config = new KafkaConfig(props(
+                "zookeeper.connect", zkConnect,
+                "broker.id", "0",
+                "log.dirs", Files.createTempDirectory("kafka-").toAbsolutePath().toString(),
+                "listeners", "PLAINTEXT://localhost:9092"));
         Time mock = new MockTime();
         kafkaServer = TestUtils.createServer(config, mock);
     }
 
-    /**
-     * Creates 2 topics (t1, t2) with different partition counts (32, 64) and fills them with items
-     *
-     * @throws IOException
-     */
-    private void fillTopics() throws IOException {
+    // Creates 2 topics (t1, t2) with different partition counts (32, 64) and fills them with items
+    private void fillTopics() {
         createTopic(zkUtils, "t1", 32, 1, new Properties(), RackAwareMode.Disabled$.MODULE$);
         createTopic(zkUtils, "t2", 64, 1, new Properties(), RackAwareMode.Disabled$.MODULE$);
 
         System.out.println("Filling Topics");
-        Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "localhost:9092");
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", IntegerSerializer.class.getName());
-        try (KafkaProducer<String, Integer> producer = new KafkaProducer<>(properties)) {
+        Properties props = props(
+                "bootstrap.servers", "localhost:9092",
+                "key.serializer", StringSerializer.class.getName(),
+                "value.serializer", IntegerSerializer.class.getName());
+        try (KafkaProducer<String, Integer> producer = new KafkaProducer<>(props)) {
             for (int i = 1; i <= MESSAGE_COUNT_PER_TOPIC; i++) {
                 producer.send(new ProducerRecord<>("t1", "t1-" + i, i));
                 producer.send(new ProducerRecord<>("t2", "t2-" + i, i));
@@ -142,25 +157,11 @@ public class ConsumeKafka {
         zkServer.shutdown();
     }
 
-    private static Properties getProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("group.id", "group-" + Math.random());
-        properties.setProperty("bootstrap.servers", "localhost:9092");
-        properties.setProperty("key.deserializer", StringDeserializer.class.getCanonicalName());
-        properties.setProperty("value.deserializer", IntegerDeserializer.class.getCanonicalName());
-        properties.setProperty("auto.offset.reset", "earliest");
-        return properties;
-    }
-
-    private static class CountingP extends AbstractProcessor {
-        private int count = 0;
-        @Override
-        protected boolean tryProcess(int ordinal, @Nonnull Object item) throws Exception {
-            count++;
-            if (count % 100_000 == 0) {
-                System.out.println(count);
-            }
-            return true;
+    private static Properties props(String... kvs) {
+        final Properties props = new Properties();
+        for (int i = 0; i < kvs.length;) {
+            props.setProperty(kvs[i++], kvs[i++]);
         }
+        return props;
     }
 }
