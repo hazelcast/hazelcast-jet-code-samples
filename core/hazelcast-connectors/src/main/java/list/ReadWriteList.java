9b4bb3514443c68e2243d8f7a711cbccc6a6e37b
Ali Gurbuz <ali@hazelcast.com>
2017-04-24 13:53:18 +0300

    added Hazelcast Connectors code samples (IMap/ICache/IList)

---
+++ core/hazelcast-connectors/src/main/java/list/ReadWriteList.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package list;
+
+import com.hazelcast.jet.DAG;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.Processors;
+import com.hazelcast.jet.Vertex;
+import com.hazelcast.jet.stream.IStreamList;
+
+import static com.hazelcast.jet.Edge.between;
+
+/**
+ * A DAG which reads from a Hazelcast Ilist,
+ * converts the item to string,
+ * and writes to another Hazelcast IList
+ */
+public class ReadWriteList {
+
+    private static final int ITEM_COUNT = 10;
+    private static final String SOURCE_LIST_NAME = "sourceList";
+    private static final String SINK_LIST_NAME = "sinkList";
+
+    public static void main(String[] args) throws Exception {
+        JetInstance instance = Jet.newJetInstance();
+
+        try {
+            IStreamList<Integer> sourceList = instance.getList(SOURCE_LIST_NAME);
+            for (int i = 0; i < ITEM_COUNT; i++) {
+                sourceList.add(i);
+            }
+
+            DAG dag = new DAG();
+
+            Vertex source = dag.newVertex("source", Processors.readList(SOURCE_LIST_NAME)).localParallelism(1);
+            Vertex transform = dag.newVertex("transform", Processors.map((Integer i) -> Integer.toString(i)));
+            Vertex sink = dag.newVertex("sink", Processors.writeList(SINK_LIST_NAME));
+
+            dag.edge(between(source, transform));
+            dag.edge(between(transform, sink));
+
+            instance.newJob(dag).execute().get();
+
+            IStreamList<String> sinkList = instance.getList(SINK_LIST_NAME);
+            System.out.println("Sink list size: " + sinkList.size());
+            System.out.println("Sink list items: ");
+            for (String s : sinkList) {
+                System.out.println(s);
+            }
+        } finally {
+            Jet.shutdownAll();
+        }
+
+    }
+}
---
+++ core/hazelcast-connectors/src/main/java/map/ReadWriteMap.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package map;
+
+import com.hazelcast.jet.DAG;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.Processors;
+import com.hazelcast.jet.Util;
+import com.hazelcast.jet.Vertex;
+import com.hazelcast.jet.stream.IStreamMap;
+
+import java.util.Map.Entry;
+
+import static com.hazelcast.jet.Edge.between;
+
+/**
+ * A DAG which reads from a Hazelcast IMap,
+ * converts the key/value to string,
+ * and writes to another Hazelcast IMap
+ */
+public class ReadWriteMap {
+
+    private static final int ITEM_COUNT = 10;
+    private static final String SOURCE_MAP_NAME = "sourceMap";
+    private static final String SINK_MAP_NAME = "sinkMap";
+
+    public static void main(String[] args) throws Exception {
+        JetInstance instance = Jet.newJetInstance();
+
+        try {
+            IStreamMap<Integer, Integer> sourceMap = instance.getMap(SOURCE_MAP_NAME);
+            for (int i = 0; i < ITEM_COUNT; i++) {
+                sourceMap.put(i, i);
+            }
+
+            DAG dag = new DAG();
+
+            Vertex source = dag.newVertex("source", Processors.readMap(SOURCE_MAP_NAME));
+            Vertex transform = dag.newVertex("transform", Processors.map((Entry<Integer, Integer> e)
+                    -> Util.entry(e.getKey().toString(), e.getValue().toString())));
+            Vertex sink = dag.newVertex("sink", Processors.writeMap(SINK_MAP_NAME));
+
+            dag.edge(between(source, transform));
+            dag.edge(between(transform, sink));
+
+            instance.newJob(dag).execute().get();
+
+            IStreamMap<String, String> sinkMap = instance.getMap(SINK_MAP_NAME);
+            System.out.println("Sink map size: " + sinkMap.size());
+            System.out.println("Sink map entries: ");
+            for (int i = 0; i < ITEM_COUNT; i++) {
+                System.out.println(sinkMap.get(Integer.toString(i)));
+            }
+        } finally {
+            Jet.shutdownAll();
+        }
+
+    }
+}
