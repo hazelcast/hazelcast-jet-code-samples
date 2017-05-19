c97276445352e673f9661ac5cf6d2f22ed1049fe
Viliam Durina <viliam-durina@users.noreply.github.com>
2017-05-19 08:23:35 +0200

    Hash join sample (#28)
    Demonstrate enrichment using:
    - HashMap distributed to all processors
    - ReplicatedMap

---
+++ batch/enrichment/src/main/java/HashJoinP.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.hazelcast.jet.AbstractProcessor;
+import com.hazelcast.jet.function.DistributedFunction;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+
+/**
+ * A processor to join input items to a map using key extracted from items. It
+ * must receive the map on ordinal 0 first, and then it can receive items on
+ * other ordinals. Useful for enriching items with additional information.
+ * <p>
+ * Output is {@code Object[] {item, enrichment}}.
+ *
+ * @param <T> Input item type
+ * @param <K> Key type
+ */
+public class HashJoinP<T, K> extends AbstractProcessor {
+
+    private final DistributedFunction<T, K> extractKeyF;
+    private Map<K, ?> map;
+
+    public HashJoinP(@Nonnull DistributedFunction<T, K> extractKeyF) {
+        this.extractKeyF = extractKeyF;
+    }
+
+    @Override
+    protected boolean tryProcess0(@Nonnull Object item) throws Exception {
+        // Joined map comes to ordinal 0
+        assert map == null;
+        map = (Map<K, ?>) item;
+        return true;
+    }
+
+    @Override
+    protected boolean tryProcess(int ordinal, @Nonnull Object item) throws Exception {
+        // items come to ordinals >= 1
+        K key = extractKeyF.apply((T) item);
+        Object o = map.get(key);
+        return tryEmit(new Object[] { item, o });
+    }
+}
---
+++ batch/enrichment/src/main/java/HashMapEnrichment.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.hazelcast.jet.DAG;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.Vertex;
+import com.hazelcast.jet.samples.enrichment.GenerateTradesP;
+import com.hazelcast.jet.samples.enrichment.TickerInfo;
+import com.hazelcast.jet.samples.enrichment.Trade;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import static com.hazelcast.jet.Edge.between;
+import static com.hazelcast.jet.Edge.from;
+import static com.hazelcast.jet.Processors.collect;
+import static com.hazelcast.jet.Processors.readMap;
+import static com.hazelcast.jet.Processors.writeLogger;
+
+/**
+ * This sample shows, how to enrich batch or stream of items with additional
+ * information by matching them by key. This version first builds a HashMap
+ * that is distributed to {@link HashJoinP}.
+ * <p>
+ * The {@link HashJoinP} expects enrichment table on input ordinal 0 and items
+ * to enrich on all other ordinals. The edge at ordinal 0 must have {@link
+ * com.hazelcast.jet.Edge#priority(int) priority} set to -1 to ensure, that
+ * items on this edge are processed before items to enrich.
+ * <p>
+ * The {@code readTickerInfoMap} reads the items in distributed way. In order
+ * to have full copy on each node we need to broadcast and distribute to a
+ * processor with local parallelism of 1. From there, we broadcast the same
+ * {@code Map} instance to all local processors. Sending locally does not copy
+ * the object, thus this will not increase memory usage.
+ * <p>
+ * The DAG is as follows:
+ * <pre>{@code
+ *                             +--------------------+
+ *                             | Read tickerInfoMap |
+ *                             +---------+----------+
+ *                                       |
+ * +-----------------+                   | TickerInfo
+ * |  Trades source  |                   |  (broadcast, distributed edge)
+ * +--------+--------+          +--------+--------+
+ *          |                   |  Squash to map  |
+ *          | Trade             +---------------+-+    (localParallelism = 1)
+ *          |  (local edge)                     |
+ * +--------+--------+                          |
+ * |      Joiner     +--------------------------+
+ * +--------+--------+                    Map<ticker, tickerInfo>
+ *          |                              (local, broadcast, priority edge)
+ *          | Object[]{trade, tradeInfo}
+ *          |
+ * +--------+--------+
+ * |      Sink       |
+ * +-----------------+
+ * }</pre>
+ */
+public class HashMapEnrichment {
+
+    private static final String TICKER_INFO_MAP_NAME = "tickerInfoMap";
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("hazelcast.logging.type", "log4j");
+
+        JetInstance instance = Jet.newJetInstance();
+        Jet.newJetInstance();
+        try {
+            TickerInfo.populateMap(instance.getMap(TICKER_INFO_MAP_NAME));
+
+            DAG dag = new DAG();
+
+            Vertex tradesSource = dag.newVertex("tradesSource", GenerateTradesP::new)
+                    .localParallelism(1);
+            Vertex readTickerInfoMap = dag.newVertex("readTickerInfoMap", readMap(TICKER_INFO_MAP_NAME));
+            Vertex collectToMap = dag.newVertex("collectToMap",
+                            collect(HashMap::new, (HashMap a, Entry e) -> a.put(e.getKey(), e.getValue())))
+                    .localParallelism(1);
+            Vertex joiner = dag.newVertex("joiner", () -> new HashJoinP<>(Trade::getTicker));
+            Vertex sink = dag.newVertex("sink", writeLogger(o -> Arrays.toString((Object[]) o)))
+                    .localParallelism(1);
+
+            dag.edge(between(readTickerInfoMap, collectToMap)
+                    .broadcast()
+                    .distributed())
+               .edge(from(collectToMap).to(joiner, 0)
+                    .broadcast()
+                    .priority(-1))
+               .edge(from(tradesSource).to(joiner, 1)
+                    .partitioned(Trade::getTicker))
+               .edge(between(joiner, sink));
+
+            instance.newJob(dag).execute().get();
+        } finally {
+            Jet.shutdownAll();
+        }
+    }
+}
