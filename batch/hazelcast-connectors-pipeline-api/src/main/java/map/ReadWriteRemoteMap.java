ef3de084b107bb7608ce4dcabcf009f6e8f3d704
Ali Gurbuz <ali@hazelcast.com>
2017-10-02 11:05:40 +0300

    add pipeline api samples for Hazelcast Connectors

---
+++ batch/hazelcast-connectors-pipeline-api/src/main/java/map/ReadWriteRemoteMap.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package map;
+
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.Pipeline;
+import com.hazelcast.jet.Sinks;
+import com.hazelcast.jet.Sources;
+import com.hazelcast.map.listener.EntryAddedListener;
+
+import static com.hazelcast.jet.Util.entry;
+
+/**
+ * Demonstrates the usage of Hazelcast IMap as source and sink
+ * from/to a remote cluster with the Pipeline API.
+ */
+public class ReadWriteRemoteMap {
+
+    static final String SOURCE_MAP_NAME = "sourceMap";
+    static final String SINK_MAP_NAME = "sinkMap";
+
+    public static void main(String[] args) throws Exception {
+        RemoteNode remoteNode = new RemoteNode();
+        remoteNode.start();
+
+        JetInstance instance = Jet.newJetInstance();
+
+        try {
+            ClientConfig clientConfig = new ClientConfig();
+            clientConfig.getGroupConfig().setName("dev").setPassword("dev-pass");
+            clientConfig.getNetworkConfig().addAddress("localhost:6701");
+
+            Pipeline pipeline = Pipeline.create();
+
+            pipeline.drawFrom(Sources.readMap(SOURCE_MAP_NAME, clientConfig))
+                    .map(e -> entry(e.getKey().toString(), e.getValue().toString()))
+                    .drainTo(Sinks.writeMap(SINK_MAP_NAME, clientConfig));
+
+            instance.newJob(pipeline).join();
+        } finally {
+            Jet.shutdownAll();
+            remoteNode.stop();
+        }
+    }
+
+    private static class RemoteNode {
+
+        private static final int ITEM_COUNT = 10;
+
+        private HazelcastInstance instance;
+
+        void start() throws Exception {
+            Config config = new Config();
+            config.getNetworkConfig().setPort(6701);
+            instance = Hazelcast.newHazelcastInstance(config);
+            IMap<Integer, Integer> sourceMap = instance.getMap(SOURCE_MAP_NAME);
+            for (int i = 0; i < ITEM_COUNT; i++) {
+                sourceMap.put(i, i);
+            }
+            IMap<String, String> sinkMap = instance.getMap(SINK_MAP_NAME);
+            sinkMap.addEntryListener((EntryAddedListener<String, String>) event
+                    -> System.out.println("Entry added to sink" + event.getKey() + "-" + event.getValue()), true);
+        }
+
+        void stop() {
+            instance.shutdown();
+        }
+    }
+}
---
+++ core-api/batch/hazelcast-connectors/src/main/java/map/ReadWriteMapWithProjectionAndPredicate.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package map;
+
+import com.hazelcast.core.IMap;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.core.DAG;
+import com.hazelcast.jet.core.Vertex;
+import com.hazelcast.jet.core.processor.SinkProcessors;
+import com.hazelcast.jet.core.processor.SourceProcessors;
+
+import java.util.Map.Entry;
+
+import static com.hazelcast.jet.Util.entry;
+import static com.hazelcast.jet.core.Edge.between;
+
+/**
+ * A DAG which reads from a Hazelcast IMap,
+ * converts the key/value to string,
+ * and writes to another Hazelcast IMap
+ */
+public class ReadWriteMapWithProjectionAndPredicate {
+
+    private static final int ITEM_COUNT = 10;
+    private static final String SOURCE_MAP_NAME = "sourceMap";
+    private static final String SINK_MAP_NAME = "sinkMap";
+
+    public static void main(String[] args) throws Exception {
+        JetInstance instance = Jet.newJetInstance();
+
+        try {
+            IMap<Integer, Integer> sourceMap = instance.getMap(SOURCE_MAP_NAME);
+            for (int i = 0; i < ITEM_COUNT; i++) {
+                sourceMap.put(i, i);
+            }
+
+            DAG dag = new DAG();
+
+            Vertex source = dag.newVertex("source", SourceProcessors.readMap(SOURCE_MAP_NAME,
+                    (Entry<Integer, Integer> e) -> e.getValue() != 0,
+                    e -> entry(e.getKey().toString(), e.getValue().toString())));
+            Vertex sink = dag.newVertex("sink", SinkProcessors.writeMap(SINK_MAP_NAME));
+
+            dag.edge(between(source, sink));
+
+            instance.newJob(dag).join();
+
+            IMap<String, String> sinkMap = instance.getMap(SINK_MAP_NAME);
+            System.out.println("Sink map size: " + sinkMap.size());
+            System.out.println("Sink map entries: ");
+            for (int i = 0; i < ITEM_COUNT; i++) {
+                System.out.println(sinkMap.get(Integer.toString(i)));
+            }
+        } finally {
+            Jet.shutdownAll();
+        }
+
+    }
+}
