356ec9750ed0bdcc179cd24afd2ad2275e0a7934
Emin Demirci <emin@hazelcast.com>
2019-09-19 16:16:34 +0300

    Delete moved code samples to main repo (#210)

--- integration/spring/src/main/resources/db/sql/insert-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users VALUES (1, 'ali', 'ali@hazelcast.com');
-INSERT INTO users VALUES (2, 'can', 'can@hazelcast.com');
-INSERT INTO users VALUES (3, 'marko', 'marko@hazelcast.com');
-INSERT INTO users VALUES (4, 'vilo', 'vilo@hazelcast.com');
