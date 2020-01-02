f35f70562cf145f67853212a99590f8ec436dc4f
Viliam Durina <viliam-durina@users.noreply.github.com>
2020-01-02 09:11:50 +0100

    Fix checkstyle

--- core-api/sliding-windows-core-api/src/main/java/StockExchangeCoreApi.java
+++ core-api/sliding-windows-core-api/src/main/java/StockExchangeCoreApi.java
@@ -136,8 +136,8 @@ public class StockExchangeCoreApi {
                                                          .atZone(ZoneId.systemDefault())),
                                 wr.getKey(), wr.getValue())
         ));
-        Vertex sink = dag.newVertex("sink",
-                SinkProcessors.writeFileP(OUTPUT_DIR_NAME, StandardCharsets.UTF_8, null, null, false, Object::toString));
+        Vertex sink = dag.newVertex("sink", SinkProcessors.writeFileP(OUTPUT_DIR_NAME, StandardCharsets.UTF_8,
+                null, null, false, Object::toString));
 
         tradeSource.localParallelism(1);
 
